cc7aa1bf6f2ce6117078747ac7e0c24a2598e835
Yash Verma <developeryash01@gmail.com>
2025-08-30 00:48:11 +0530

    Implemented Controller Service and Repository layer in the project

---
+++ notes/src/main/java/com/secure/notes/models/Note.java
@@ -0,0 +1,45 @@
+package com.secure.notes.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String content;
+
+    private String ownerUsername;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getOwnerUsername() {
+        return ownerUsername;
+    }
+
+    public void setOwnerUsername(String ownerUsername) {
+        this.ownerUsername = ownerUsername;
+    }
+}
